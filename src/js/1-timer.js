62b746db35a578c7dbbe6b640efeb222ac99f1eb
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-07 16:17:17 +0200

    edit

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -2,6 +2,16 @@ import flatpickr from 'flatpickr';
 
 import 'flatpickr/dist/flatpickr.min.css';
 
+import iziToast from 'izitoast';
+
+import 'izitoast/dist/css/iziToast.min.css';
+
+flatpickr(input, options);
+iziToast.show({
+  title: 'Hey',
+  message: 'Please choose a date in the future',
+});
+
 const input = document.querySelector('#datetime-picker');
 let userSelectedDate;
 const startBtn = document.querySelector('[data-start]');
@@ -21,7 +31,26 @@ const options = {
     userSelectedDate = selectedDates[0];
   },
 };
-
-flatpickr(input, options);
-
+startBtn.addEventListener('click', onBtnClick);
 function onBtnClick() {}
+
+function addLeadingZero(value) {}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
