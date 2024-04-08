16b0a982606cbd926546f5f7c58b3746c833276d
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-08 16:16:42 +0200

    edit-1task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -18,7 +18,7 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0]);
+    // console.log(selectedDates[0]);
     if (selectedDates[0] < Date.now()) {
       iziToast.show({
         title: 'Error',
@@ -27,56 +27,55 @@ const options = {
     }
     startBtn.disabled = false;
     userSelectedDate = selectedDates[0];
-    function addLeadingZero(value) {
-      return String(value).padStart(2, '0');
-    }
-
-    function convertMs(ms) {
-      // Number of milliseconds per unit of time
-      const second = 1000;
-      const minute = second * 60;
-      const hour = minute * 60;
-      const day = hour * 24;
-
-      // Remaining days
-      const days = Math.floor(ms / day);
-      // Remaining hours
-      const hours = Math.floor((ms % day) / hour);
-      // Remaining minutes
-      const minutes = Math.floor(((ms % day) % hour) / minute);
-      // Remaining seconds
-      const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-
-      return { days, hours, minutes, seconds };
-    }
-
-    startBtn.addEventListener('click', onBtnClick);
-    function onBtnClick() {
-      const intervalId = setInterval(() => {
-        const currentTime = Date.now();
-        startBtn.disabled = true;
-        const diff = selectedDates[0] - currentTime;
-        console.log(diff);
-        const { days, hours, minutes, seconds } = convertMs(diff);
-        if (diff > 0) {
-          document.querySelector('[data-days]').textContent =
-            addLeadingZero(days);
-          document.querySelector('[data-hours]').textContent =
-            addLeadingZero(hours);
-          document.querySelector('[data-minutes]').textContent =
-            addLeadingZero(minutes);
-          document.querySelector('[data-seconds]').textContent =
-            addLeadingZero(seconds);
-        } else {
-          clearInterval(intervalId);
-          document.querySelector('[data-days]').textContent = '00';
-          document.querySelector('[data-hours]').textContent = '00';
-          document.querySelector('[data-minutes]').textContent = '00';
-          document.querySelector('[data-seconds]').textContent = '00';
-        }
-      }, 1000);
-    }
+    // console.log(userSelectedDate);
   },
 };
+console.log(userSelectedDate);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+startBtn.addEventListener('click', onBtnClick);
+function onBtnClick() {
+  // console.log(userSelectedDate);
+  const intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    startBtn.disabled = true;
+    const diff = userSelectedDate - currentTime;
+    // console.log(diff);
+    const { days, hours, minutes, seconds } = convertMs(diff);
+    if (diff > 0) {
+      document.querySelector('[data-days]').textContent = addLeadingZero(days);
+      document.querySelector('[data-hours]').textContent =
+        addLeadingZero(hours);
+      document.querySelector('[data-minutes]').textContent =
+        addLeadingZero(minutes);
+      document.querySelector('[data-seconds]').textContent =
+        addLeadingZero(seconds);
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
 
 flatpickr(input, options);
