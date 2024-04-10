026aae206a26545aa6c7e859edf1ac1eede6f83d
Kyryl2 <krasiuk.4884@gmail.com>
2024-04-10 15:40:57 +0200

    edit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -2,58 +2,28 @@ import iziToast from 'izitoast';
 
 import 'izitoast/dist/css/iziToast.min.css';
 
-// const form = document.querySelector('.form');
-// form.addEventListener('submit', onSubmitForm);
+const form = document.querySelector('.form');
+form.addEventListener('submit', onSubmitForm);
+// ----Way 1----//
 
-// function onSubmitForm(event) {
-//   event.preventDefault();
-
-//   const delay = document.querySelector('input[name="delay"]').value;
-//   const checkBox = document.querySelector('input[name="state"]:checked').value;
-//   return new Promise((resolve, reject) => {
-//     if (checkBox === 'fulfilled') {
-//       setTimeout(() => {
-//         resolve(delay);
-//       }, delay);
-//     }
-//     if (checkBox === 'rejected') {
-//       setTimeout(() => {
-//         reject(delay);
-//       }, delay);
-//     }
-//   });
-// }
-// onSubmitForm()
-//   .then(result =>
-//     iziToast.show({
-//       message: `✅ Fulfilled promise in ${result}ms`,
-//       position: 'topRight',
-//       color: 'green',
-//     })
-//   )
-//   .catch(error =>
-//     iziToast.show({
-//       message: `❌ Rejected promise in ${error}ms`,
-//       position: 'topRight',
-//       color: 'red',
-//     })
-//   );
-
-document.querySelector('.form').addEventListener('submit', function (event) {
+function createPromise(state, delay) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else if (state === 'rejected') {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
+function onSubmitForm(event) {
   event.preventDefault();
 
   const delay = document.querySelector('input[name="delay"]').value;
   const checkBox = document.querySelector('input[name="state"]:checked').value;
 
-  const customPromise = new Promise((resolve, reject) => {
-    if (checkBox === 'fulfilled') {
-      setTimeout(() => resolve(delay), delay);
-    } else if (checkBox === 'rejected') {
-      setTimeout(() => reject(delay), delay);
-    }
-  });
-
-  customPromise
+  createPromise(checkBox, delay)
     .then(result =>
       iziToast.show({
         message: `✅ Fulfilled promise in ${result}ms`,
@@ -61,7 +31,6 @@ document.querySelector('.form').addEventListener('submit', function (event) {
         color: 'green',
       })
     )
-
     .catch(error =>
       iziToast.show({
         message: `❌ Rejected promise in ${error}ms`,
@@ -69,4 +38,38 @@ document.querySelector('.form').addEventListener('submit', function (event) {
         color: 'red',
       })
     );
-});
+}
+
+//---Way 2---//
+
+// document.querySelector('.form').addEventListener('submit', function (event) {
+//   event.preventDefault();
+
+//   const delay = document.querySelector('input[name="delay"]').value;
+//   const checkBox = document.querySelector('input[name="state"]:checked').value;
+
+//   const customPromise = new Promise((resolve, reject) => {
+//     if (checkBox === 'fulfilled') {
+//       setTimeout(() => resolve(delay), delay);
+//     } else if (checkBox === 'rejected') {
+//       setTimeout(() => reject(delay), delay);
+//     }
+//   });
+
+//   customPromise
+//     .then(result =>
+//       iziToast.show({
+//         message: `✅ Fulfilled promise in ${result}ms`,
+//         position: 'topRight',
+//         color: 'green',
+//       })
+//     )
+
+//     .catch(error =>
+//       iziToast.show({
+//         message: `❌ Rejected promise in ${error}ms`,
+//         position: 'topRight',
+//         color: 'red',
+//       })
+//     );
+// });
